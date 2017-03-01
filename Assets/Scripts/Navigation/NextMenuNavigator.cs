70d6f52a7f36c66418f6d8d3e541e63715f98ed4
bengreenier <ben@bengreenier.com>
2017-03-01 17:43:23 -0500

    fix #6

--- Assets/Scripts/Navigation/NextMenuNavigator.cs
+++ Assets/Scripts/Navigation/NextMenuNavigator.cs
@@ -25,22 +25,30 @@ namespace MenuStack.Navigation
         /// </summary>
         /// <param name="root">the root level transform to start matching</param>
         /// <param name="current">the current menu, we want the one after this</param>
+        /// <param name="hasSeenCurrent">indicates if we've yet passed <c>current</c> in the tree</param>
         /// <returns>a matched <see cref="Menu"/>, or <c>null</c></returns>
-        public Menu FindFirstRecursively(Transform root, Menu current)
+        public Menu FindFirstRecursively(Transform root, Menu current, bool hasSeenCurrent = false)
         {
             for (var i = 0; i < root.childCount; i++)
             {
                 var child = root.GetChild(i);
                 var menu = child.GetDerivedMenuComponent();
 
-                if (menu != null && menu != current)
+                if (menu != null)
                 {
-                    return menu;
+                    if (menu == current)
+                    {
+                        hasSeenCurrent = true;
+                    }
+                    else if (menu != current && hasSeenCurrent)
+                    {
+                        return menu;
+                    }
                 }
 
                 if (child.childCount > 0)
                 {
-                    var res = FindFirstRecursively(child, current);
+                    var res = FindFirstRecursively(child, current, hasSeenCurrent);
 
                     if (res != null)
                     {
